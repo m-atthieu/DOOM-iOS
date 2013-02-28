d4587ef8308f96118f6a3201a71c272ad7068a04
Matthieu DESILE <matthieu.desile@gmail.com>
2013-02-28 12:01:09 +0100

    modified script that create missing assets

--- IB Images/missing-images.rb
+++ IB Images/missing-images.rb
@@ -10,19 +10,25 @@ File.readlines('missing-images.txt').each do |line|
         size = size.collect{ |i| i.to_i }
     end
     bn = File.basename(name, '.png').split('_').join(' ')
-    unless size.nil?
-        # check if the existing image is a dummy image or not
-        if File.exists?(name) then
-            cmd = "identify -verbose #{name} | grep 'title: dummy'"
-            dummy = `#{cmd}`
-        else
-            dummy = 'dummy'
-        end
 
-        puts "#{bn} #{name} #{size}"
-        cmd = "echo '#{bn}' | convert -set title 'dummy' -size #{size[0]}x#{size[1]} -page #{size[0]}x#{size[1]}+2+2 -background wheat -fill black -pointsize 12 text:- +repage #{name}"
-        if dummy != '' or not File.exists?(name) then
-            system cmd
-        end
+    # check if the existing image is a dummy image or not
+    if File.exists?(name) then
+        cmd = "identify -verbose #{name} | grep 'title: dummy'"
+        dummy = `#{cmd}`
+    else
+        dummy = 'dummy'
     end
+
+    if size.nil? then
+        size = [64, 64]
+        bgcolor = 'red'
+    else
+        bgcolor = 'wheat'
+    end
+
+    cmd = "echo '#{bn}' | convert -set title 'dummy' -size #{size[0]}x#{size[1]} -page #{size[0]}x#{size[1]}+2+2 -border 1x1 -bordercolor grey -background #{bgcolor} -fill black -pointsize 12 text:- +repage #{name}"
+    if dummy != '' or not File.exists?(name) then
+        system cmd
+    end
+
 end
